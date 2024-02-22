677b4cc2dc9038fb277d3436b5768aeb3b9939a8
Cameron Browne <brownecameron18@gmail.com>
2024-02-22 22:34:08 +0000

    created and styled the comments

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import NavBar from "./components/NavBar/NavBar.js";
 import VideoPlayer from "./components/VideoPlayer/VideoPlayer.js";
 import VideoDetails from "./components/VideoDetails/VideoDetails.js";
 import CommentForm from "./components/CommentForm/CommentForm.js";
-
+import Comments from "./components/Comments/Comments.js";
 // IMPORT VIDEOS
 import VideoList from "./data/video-details.json";
 
@@ -35,9 +35,7 @@ function App() {
         <section className="main-section1">
           <VideoDetails video={selectedVideo} />
           <CommentForm video={selectedVideo} />
-          {/*}
           <Comments video={selectedVideo} />
-           */}
 
         </section>
 {/*}
---
+++ src/components/Comments/Comments.js
@@ -0,0 +1,29 @@
+import "./Comments.scss";
+
+function convertDate(timestamp) {
+    var time = new Date(timestamp);
+    return (time.toLocaleDateString())
+}
+
+function Comments({video}) {
+  return (
+    <section className="comment-list">
+      {video.comments.map((comment) => (
+        <article className="comment" key={comment.id}>
+          <div className="comment__icon"></div>
+          <div className="comment__main">
+            <div className="comment__main-namedate">
+              <h3 className="comment__name">{comment.name}</h3>
+              <span className="comment__date">
+                {convertDate(comment.timestamp)}
+              </span>
+            </div>
+            <p className="comment__text">{comment.comment}</p>
+          </div>
+        </article>
+      ))}
+    </section>
+  );
+}
+
+export default Comments;
