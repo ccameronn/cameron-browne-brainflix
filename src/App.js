19f14bc394c3454766125e41505904396d45ec25
Cameron Browne <brownecameron18@gmail.com>
2024-02-22 21:55:51 +0000

    added and styled video details

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { useState } from "react";
 // IMPORT COMPONENTS
 import NavBar from "./components/NavBar/NavBar.js";
 import VideoPlayer from "./components/VideoPlayer/VideoPlayer.js";
-// import VideoDetails from "./components/VideoDetails/VideoDetails.js";
-
+import VideoDetails from "./components/VideoDetails/VideoDetails.js";
+import CommentForm from "./components/CommentForm/CommentForm.js";
 
 // IMPORT VIDEOS
 import VideoList from "./data/video-details.json";
@@ -29,23 +29,28 @@ function App() {
     <div className="App">
 
     <NavBar />
-
     <VideoPlayer video={selectedVideo} />
 
+    <main className="main-section">
+        <section className="main-section1">
+          <VideoDetails video={selectedVideo} />
+          <CommentForm video={selectedVideo} />
+          {/*}
+          <Comments video={selectedVideo} />
+           */}
+
+        </section>
+{/*}
+        <SideVideos filteredVideo={filteredVideo} />
+      */}
+      </main>
 
 
   {/*}
 
 
 
-      <main className="main-section">
-        <section className="main-section1">
-          <VideoDetails video={selectedVideo} />
-          <CommentForm video={selectedVideo} />
-          <Comments video={selectedVideo} />
-        </section>
-        <SideVideos filteredVideo={filteredVideo} />
-      </main>
+      
   */}
   
     </div>
---
+++ src/components/VideoDetails/VideoDetails.js
@@ -0,0 +1,34 @@
+import "./VideoDetails.scss";
+
+import viewsIcon from "../../assets/images/icons/views.svg";
+import likesIcon from "../../assets/images/icons/likes.svg";
+
+function VideoDetails({ video }) {
+
+    var timestamp = new Date(video.timestamp);
+
+  return (
+    <section className="active-video-details">
+      <h1 className="active-video-details__title">{video.title}</h1>
+      <article className="stats">
+        <div className="stats__left">
+          <p className="stats__channel">By {video.channel}</p>
+          <p className="stats__date">{timestamp.toLocaleDateString()}</p>
+        </div>
+        <div className="stats__right">
+          <div className="stats__views">
+            <img src={viewsIcon} className="stats__views--icon"></img>
+            <p className="stats__views--count">{video.views}</p>
+          </div>
+          <div className="stats__likes">
+            <img src={likesIcon} className="stats__likes--icon"></img>
+            <p className="stats__likes--count">{video.likes}</p>
+          </div>
+        </div>
+      </article>
+      <p className="active-video-details__description">{video.description}</p>
+    </section>
+  );
+}
+
+export default VideoDetails;
