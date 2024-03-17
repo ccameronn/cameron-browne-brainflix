d47778767c58f2bde529c87579f142b986bb5c55
Cameron Browne <brownecameron18@gmail.com>
2024-03-17 18:06:29 +0000

    prep api integration

--- src/pages/VideoPage/VideoPage.js
+++ src/pages/VideoPage/VideoPage.js
@@ -1,6 +1,6 @@
 import './VideoPage.scss';
 import { useState , useEffect } from "react";
-import { useParams , useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 // IMPORT COMPONENTS
 import VideoPlayer from "../../components/VideoPlayer/VideoPlayer.js";
