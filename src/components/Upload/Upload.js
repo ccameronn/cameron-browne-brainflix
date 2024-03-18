831d4a6756b47412978beeb96798c79e6d22aedd
Cameron Browne <brownecameron18@gmail.com>
2024-03-18 16:41:45 +0000

    fix banches - redo api calls

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -26,43 +26,41 @@ function Upload() {
     
     <section className="upload-section">
       <h1 className="upload-section__title">Upload Video</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="upload-form">
+      <form className="upload-form" onSubmit={handleSubmit}>
         <label htmlFor="upload-image" className="upload-form__label upload-form__image-container">
-        VIDEO THUMBNAIL
-        <img
-          src={uploadImage}
-          id="upload-image"
-          className="upload-form__image"
-          alt="Upload Thumbnail"
-        />
-      </label>
-      <div className="upload-form__inputs">
-        <label htmlFor="upload-title" className="upload-form__label">
-          TITLE YOUR VIDEO
-          <input
-            placeholder="Add a title to your video"
-            type="text"
-            id="upload-title"
-            className="upload-form__input"
-            value={title}
-            onChange={handleTitleChange}
+          VIDEO THUMBNAIL
+          <img
+            src={uploadImage}
+            id="upload-image"
+            className="upload-form__image"
+            alt="Upload Thumbnail"
           />
         </label>
-        <label htmlFor="upload-description" className="upload-form__label">
-          ADD A VIDEO DESCRIPTION
-          <textarea
-            placeholder="Add a description to your video"
-            rows="5"
-            required
-            type="text"
-            id="upload-description"
-            className="upload-form__input"
-            value={description}
-            onChange={handleDescriptionChange}
-          />
-        </label>
-      </div>
+        <div className="upload-form__inputs">
+          <label htmlFor="upload-title" className="upload-form__label">
+            TITLE YOUR VIDEO
+            <input
+              placeholder="Add a title to your video"
+              type="text"
+              id="upload-title"
+              className="upload-form__input"
+              value={title}
+              onChange={handleTitleChange}
+            />
+          </label>
+          <label htmlFor="upload-description" className="upload-form__label">
+            ADD A VIDEO DESCRIPTION
+            <textarea
+              placeholder="Add a description to your video"
+              rows="5"
+              required
+              type="text"
+              id="upload-description"
+              className="upload-form__input"
+              value={description}
+              onChange={handleDescriptionChange}
+            />
+          </label>
         </div>
         <div className="upload-section__buttons">
           <button type="submit" className="upload-section__publish">
--- src/pages/VideoPage/VideoPage.js
+++ src/pages/VideoPage/VideoPage.js
@@ -1,7 +1,7 @@
 import './VideoPage.scss';
 import { useState , useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { axios } from "axios";
+import axios from "axios";
 
 // IMPORT COMPONENTS
 import VideoPlayer from "../../components/VideoPlayer/VideoPlayer.js";
@@ -14,29 +14,7 @@ import SideVideos from "../../components/SideVideos/SideVideos.js";
 import VideoListDetails from "../../data/video-details.json";
 import VideoList from "../../data/videos.json";
 
-
-// api call data functions
-
-const url = "https://project-2-api.herokuapp.com"
-const apiKey = "?api_key=" + "dfbe66f6-bc9d-4510-97aa-90a56ae5f1b0"
-
-const fetchVideos = async () => {
-  try {
-    return await axios.get(url + "/videos" + apiKey)
-
-  } catch (error) {
-    console.log("fetchVideos API request failed")
-  }
-}
-
-const fetchVideoDetails = async (videoId) => {
-  try {
-    return await axios.get(url + "/videos/" + videoId + apiKey)
-
-  } catch (error) {
-    console.log("fetchVideosDetails API request failed")
-  }
-}
+// FUNCTIONS TO CALL API
 
 
 function VideoPage() {
@@ -47,12 +25,9 @@ function VideoPage() {
   const [commentCount, setCommentCount] = useState(0);
 
 
-  // fetch data
 
 
-
-
-  // When videoId is changed, rerender selectedVideo
+// When videoId is changed, rerender selectedVideo
 
   const { videoId } = useParams();
 
